f44244aedcca955bfa21b0270870e8a54ebb6472
21shield <21netaly@gmail.com>
2020-08-03 17:09:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    });
+
 }
+
+addingEventListener()
+// wont activate the event on the page if you dont call it ahahaha
